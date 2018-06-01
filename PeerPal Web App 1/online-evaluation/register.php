d6c8d5aad6a0899715a04597c76a2146792b1751
VictoriaBendiii <2160316@slu.edu.ph>
2018-06-01 14:46:20 +0800

    Edited register.php and the database

--- PeerPal Web App 1/online-evaluation/register.php
+++ PeerPal Web App 1/online-evaluation/register.php
@@ -1,40 +1,41 @@
 <?php include('connection.php');?>
 <html>
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="description" content="online evaluation">
-	<meta name="author" content="Blessie Gelidon">
-	<title>Register</title>
-	<link rel="stylesheet" type="text/css" href="css/main-style.css" />
+    <meta charset="UTF-8">
+  
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="css/rstyle.css" />
+    <link rel="stylesheet" type="text/css" href="css/main-style.css" />
 </head>
+
 <body>
-	<center>
-		<div class="wrapper">
-			<div class="header">
-				<table>
-					<tr>
-						<td>Online Evaluation</td>
-						<!--<td align="right"><div id="sign-in"><a href="">Contact Us</a></div></td>-->
-					</tr>
-				</table>
-			</div>
-			<div class="body">
-				<?php
+    <center>
+        <div class="wrapper">
+            <div class="form_wrapper">
+            <div class="form_container">
+                <div class="title_container">
+                    <h2>Registration</h2>
+                </div>
+                <div class="row clearfix">
+                    <div class="">
+                        <?php
 					if(!empty($_POST['username']) && !empty($_POST['password']))
 					{
 						$username = mysqli_real_escape_string($conn, $_POST['username']);
 						$password = mysqli_real_escape_string($conn, $_POST['password']);
 						$firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
 						$lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+                        $identification = mysqli_real_escape_string($conn, $_POST['identification']);
 
-						$query = "INSERT INTO `users`(`username`, `password`, `firstname`, `lastname`) VALUES ('$username', '$password','$firstname','$lastname')";
+						$query = "INSERT INTO `users`(`username`, `password`, `firstname`, `lastname`, `identification`) VALUES ('$username', '$password','$firstname','$lastname', '$identification')";
 						$resulta = mysqli_query($conn, $query);
 						if($resulta)
 						{
 							echo "Sucessfully Registered new account.";
 						?>
-							<a href="<?php echo "join-signin.php" ?>">Login</a>
-						<?php
+                            <a href="<?php echo " join-signin.php " ?>">Login</a>
+                            <?php
 						}
 						else
 						{
@@ -44,39 +45,45 @@
 					else
 					{
 				?>
-				<form action="register.php" method="post">
-					<table id="sign-in-table">
-						<tr>
-							<td><span style="font-size: 30px;">Register</span></td>
-						</tr>
-						<tr>
-							<td><input name="username" id="username" type="text" placeholder="Username / School I.D."></td>
-						</tr>
-						<tr>
-							<td><input name="password" id="password" type="password" placeholder="Password"></td>
-						</tr>
-						<tr>
-							<td><input name="firstname" id="username" type="text" placeholder="Firstname"></td>
-						</tr>
-						<tr>
-							<td><input name="lastname" id="username" type="text" placeholder="Lastname"></td>
-						</tr>
-						<tr>
-							<td><a style=" text-decoration: none; color: #000000; font-size: 12px;" href="">By Registering you Agree to our Terms and Conditions.</a></td>
-						</tr>
-						<tr>
-							<td></td>
-						</tr>
-						<tr>
-							<td><input id="submit" type="submit" value="Register"></td>
-						</tr>
-					</table>
-				</form>
-				<?php
+                                <form action="register.php" method="post">
+                                    <div class="input_field"> <span><i aria-hidden="true" class="fa fa-envelope"></i></span>
+                                        <input type="username" name="username" placeholder="Username or ID Number" required />
+                                    </div>
+                                    <div class="input_field"> <span><i aria-hidden="true" class="fa fa-lock"></i></span>
+                                        <input type="password" name="password" placeholder="Password" required />
+                                    </div>
+                                    <div class="input_field"> <span><i aria-hidden="true" class="fa fa-lock"></i></span>
+                                        <input type="password" name="password" placeholder="Re-type Password" required />
+                                    </div>
+                                    <div class="row clearfix">
+                                        <div class="col_half">
+                                            <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
+                                                <input type="text" name="firstname" placeholder="First Name" />
+                                            </div>
+                                        </div>
+                                        <div class="col_half">
+                                            <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
+                                                <input type="text" name="lastname" placeholder="Last Name" required />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="input_field radio_option">
+                                        <input type="radio" name="identification" id="rd1" value="teacher">
+                                        <label for="rd1">Teacher</label>
+                                        <input type="radio" name="identification" id="rd2" value="student">
+                                        <label for="rd2">Student</label>
+                                    </div>
+                                    <input class="button" type="submit" value="Register" />
+                                </form>
+                    </div>
+                </div>
+            </div>
+            <?php
 					}
 				?>
-			</div>
-		</div>
-	</center>
+        </div>
+        </div>
+    </center>
 </body>
+
 </html>
