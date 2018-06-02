8b0704fa5887a6a327c76184dfd29677929b7a46
Andres, Erythrina <2160051@slu.edu.ph>
2018-06-02 16:57:59 +0800

    Edited uploadForm, added session handling

--- formBuilder Trial/uploadForm.php
+++ formBuilder Trial/uploadForm.php
@@ -1,4 +1,4 @@
-<!--<?php include('connection.php');?>-->
+<?php include('connection.php');?>
 <html>
     <head>
         <meta name=viewport content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
@@ -13,8 +13,17 @@
     <body>
     <div id='uploadContainer'>
         <form action="upload.php" method="POST" enctype="multipart/form-data" class="file-upload">
-            <label for="classCode" id="classCode">Class Code: </label>
-            <input type="text" id="classCode" name="classCode" placeholder="9358A"><br>
+            <label for="dueDate" id="dueDate">Due Date: </label>
+            <input type="date" name="due" id="dueDate" /><br>
+            <?php
+              $user = $_SESSION['username'];
+              $query = mysqli_query($conn, "SELECT * from users JOIN user_course USING(id) JOIN group_form ON courseCode = courseCodeForm JOIN form USING(formID) WHERE username = $user AND identification = 'teacher'");
+
+              while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+                echo $row['groupID'];
+              }
+            ?>
+            <div id="groupBox" style="margin-bottom: 50px;"></div><br>
             <label for="upload" class="btn">Choose a file</label>
             <input type="file" name="upload" accept="application/json" id="upload" style="display: none;" />
             <p class="file-name" style="margin-left: 230px;">Please select a JSON file</p>
