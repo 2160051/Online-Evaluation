6aa834e42d9e00d79dfaa884d0b387549433996b
2166244 <35590223+2166244@users.noreply.github.com>
2018-06-02 16:57:44 +0800

    added file for classroom.php

--- PeerPal Web App 1/online-evaluation/classroom.php
+++ PeerPal Web App 1/online-evaluation/classroom.php
@@ -1,33 +1,55 @@
 <?php include('connection.php');?>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="description" content="online evaluation">
-	<meta name="author" content="Blessie Gelidon">
-	<title>Classes</title>
-	<link rel="stylesheet" type="text/css" href="css/main-style.css" />
+  <meta charset="UTF-8">
+  <title>Online Evaluation (SLU)</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>
+      <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-	<center>
-		<div class="wrapper">
-			<div class="header">
-				<table>
-					<tr>
-						<td><b>Online Evaluation</b> <i style=" font-size: 20px;">classroom</i></td>
-						<td align="right"><a id="add" title="Press to Create or Add class" href="">+</a></td>
-						<td align="right" title="You"><?php echo $_SESSION['firstname']." ".$_SESSION['lastname']; ?></td>
-						<td align="right"><div title="Please don't go" id="sign-in"><a href="signout.php">Sign Out</a></div></td>
-					</tr>
-				</table>
-			</div>
-			<div class="body">
-				<table align="left" id="welcome">
-					<tr>
-						<td><i>Welcome</i></td>
-					</tr>
-				</table>
-			</div>
-		</div>
-	</center>
+<div class="offcanvas">
+  <div class="offcanvas__container"><a class="offcanvas__exit-overlay" href="#" data-toggle="offcanvas" data-target="exit"></a>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header navbar-header--double-btn">
+          <button class="navbar-toggle navbar-toggle--left" type="button" data-toggle="offcanvas" data-target="left" aria-expanded="false"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand navbar-brand--center" href="#">Online Evaluation</a>
+        </div>
+        <div class="navbar-offcanvas offcanvas__content offcanvas__content--left">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span>&nbsp;Home</a></li>
+            <li class=""><a href="#"><span class="glyphicon glyphicon-plus"></span>&nbsp;Add a Class</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+      
+    <div class="container">
+        <div class="page"
+        <div class="col-sm-2">
+            <h1>&nbsp; <?php               
+                $user = mysqli_real_escape_string($conn, $_SESSION['username']);
+                
+                $sql = "select * from user_course join users using(id) join course using(courseCode) where users.username =  '$user'";
+                    $result = mysqli_query($conn, $sql);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        // output data of each row
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<div>" .$row["courseCode"].  "<br>" . $row["courseName"]. "<br> <br>";
+                        }
+                    } else {
+                        echo "0 results";
+                }
+                ?></h1>
+        </div>
+    </div>
+  </div>
+</div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
+    <script  src="js/index.js"></script>
 </body>
 </html>
