780691021e9a51967fce597d8416f1f1059c6de9
Andres, Erythrina <2160051@slu.edu.ph>
2018-06-05 11:58:36 +0800

    Uploaded latest peer.sql

--- peerpal.sql
+++ peerpal.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: peerpal
 -- ------------------------------------------------------
--- Server version	5.7.14
+-- Server version	5.7.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -38,7 +38,7 @@ CREATE TABLE `course` (
 
 LOCK TABLES `course` WRITE;
 /*!40000 ALTER TABLE `course` DISABLE KEYS */;
-INSERT INTO `course` VALUES ('9358A','Web Technologies Lecture','Intro to web technologies','3:00 - 4:00 WS'),('9362','Network Lec','Administration and Maintainance','11:00 - 12:30 TF'),('9364B','Web Technologies Laboratory','Application of Technology','1:00 - 2:30 TF');
+INSERT INTO `course` VALUES ('9358A','Web Technologies Lecture','Intro to web technologies','3:00 - 4:00 WS');
 /*!40000 ALTER TABLE `course` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,9 +54,10 @@ CREATE TABLE `form` (
   `formName` varchar(140) NOT NULL,
   `formDesc` varchar(1000) DEFAULT 'No description available',
   `due` date NOT NULL,
+  `path` varchar(240) NOT NULL,
   PRIMARY KEY (`formID`),
   UNIQUE KEY `formID_UNIQUE` (`formID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,6 +66,7 @@ CREATE TABLE `form` (
 
 LOCK TABLES `form` WRITE;
 /*!40000 ALTER TABLE `form` DISABLE KEYS */;
+INSERT INTO `form` VALUES (1,'Web Technology Peer Eval Prelims','This is the peer evaluation form for Web Technology Prelims','2018-01-28','form');
 /*!40000 ALTER TABLE `form` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,10 +80,9 @@ DROP TABLE IF EXISTS `group`;
 CREATE TABLE `group` (
   `groupID` int(11) NOT NULL AUTO_INCREMENT,
   `groupNo` int(11) NOT NULL,
-  `schedule` varchar(45) NOT NULL,
   PRIMARY KEY (`groupID`),
   UNIQUE KEY `groupID_UNIQUE` (`groupID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,6 +91,7 @@ CREATE TABLE `group` (
 
 LOCK TABLES `group` WRITE;
 /*!40000 ALTER TABLE `group` DISABLE KEYS */;
+INSERT INTO `group` VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6);
 /*!40000 ALTER TABLE `group` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -104,11 +106,14 @@ CREATE TABLE `group_form` (
   `id` int(11) NOT NULL,
   `groupID` int(11) NOT NULL,
   `courseCodeForm` varchar(45) NOT NULL,
+  `formID` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `groupID_idx` (`groupID`),
   KEY `groupIDForm_idx` (`groupID`),
   KEY `courseCodeForm_idx` (`courseCodeForm`),
+  KEY `formID_idx` (`formID`),
   CONSTRAINT `courseCodeForm` FOREIGN KEY (`courseCodeForm`) REFERENCES `course` (`courseCode`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `formID` FOREIGN KEY (`formID`) REFERENCES `form` (`formID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `groupID` FOREIGN KEY (`groupID`) REFERENCES `group` (`groupID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -119,6 +124,7 @@ CREATE TABLE `group_form` (
 
 LOCK TABLES `group_form` WRITE;
 /*!40000 ALTER TABLE `group_form` DISABLE KEYS */;
+INSERT INTO `group_form` VALUES (1,1,'9358A',1),(2,2,'9358A',1),(3,3,'9358A',1),(4,4,'9358A',1),(5,5,'9358A',1),(6,6,'9358A',1);
 /*!40000 ALTER TABLE `group_form` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,7 +149,7 @@ CREATE TABLE `user_course` (
   CONSTRAINT `courseCode` FOREIGN KEY (`courseCode`) REFERENCES `course` (`courseCode`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `groupIDUser` FOREIGN KEY (`groupID`) REFERENCES `group` (`groupID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -152,7 +158,7 @@ CREATE TABLE `user_course` (
 
 LOCK TABLES `user_course` WRITE;
 /*!40000 ALTER TABLE `user_course` DISABLE KEYS */;
-INSERT INTO `user_course` VALUES (1,4,'9358A',NULL),(2,5,'9364B',NULL),(3,5,'9362',NULL);
+INSERT INTO `user_course` VALUES (1,1,'9358A',NULL);
 /*!40000 ALTER TABLE `user_course` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -169,10 +175,9 @@ CREATE TABLE `users` (
   `password` varchar(20) NOT NULL,
   `firstname` varchar(60) NOT NULL,
   `lastname` varchar(60) NOT NULL,
-  `identification` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `identification` enum('student','teacher') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -181,7 +186,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'2165500','admin','Mary','Gelidon',''),(2,'0000','admin','test','test',''),(3,'2163054','2163054','2163054','2163054',''),(4,'2160316','password','Victoria','Buse','student'),(5,'2166244','password','Chari','Parayno','student');
+INSERT INTO `users` VALUES (1,'2165500','admin','Mary','Gelidon','teacher'),(2,'0000','admin','test','test','teacher'),(3,'2163054','2163054','2163054','2163054','student'),(4,'2160316','password','Victoria','Buse','student');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -194,4 +199,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-02 16:59:17
+-- Dump completed on 2018-06-05 11:55:33
